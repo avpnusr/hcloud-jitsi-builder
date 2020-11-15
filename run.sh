e56a8408d2b110d3d6b41e73cf5caba203b3251f
aCodingFool <fool@nomail.com>
2020-11-15 21:31:16 +0100

    .

--- run.sh
+++ run.sh
@@ -12,6 +12,12 @@ else
 	echo -e "  ${GREEN}OK${NC} - already installed."
 fi
 
+cd /root
+git clone https://github.com/jitsi/docker-jitsi-meet && cd docker-jitsi-meet
+cp env.example .en
+./gen-passwords.sh
+mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
+docker-compose up -d
 
 
 	
