835c8dc6825c550945ed74267ca9865b1da15b88
aCodingFool <fool@nomail.com>
2020-11-16 21:08:09 +0100

    ...

--- run.sh
+++ run.sh
@@ -28,7 +28,7 @@ git clone https://github.com/jitsi/docker-jitsi-meet && cd docker-jitsi-meet
 cp env.example .env
 ./gen-passwords.sh
 mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
-docker-compose up -d
+# docker-compose up -d
 
 
 	
