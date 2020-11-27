72e02e3c24f90058640f8ae95888779a51a78303
avpnusr <43516996+avpnusr@users.noreply.github.com>
2020-11-27 08:58:49 +0100

    updates

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 
 
-SERVERNAME=test002
-SERVERTYPE=cx31
-SSHPUBKEY=~/.ssh/hetzner_key.pub
-SSHPRIVKEY=~/.ssh/hetzner_key
-GITREPO=https://github.com/DXorSX/hcloud-jitsi-builder.git
-SRVSTATUS=`hcloud server list -o columns=status -o noheader`
-HCCONTEXT=`hcloud context active`
+SERVERNAME=jitspriv
+SERVERTYPE=ccx31
+SSHPUBKEY=~/.ssh/id_rsa.pub
+SSHPRIVKEY=~/.ssh/id_rsa
+GITREPO=https://github.com/avpnusr/hcloud-jitsi-builder.git
+SRVSTATUS=$(hcloud server list)
+HCCONTEXT=$(hcloud context active)
 
 if [ ! -r "$SSHPRIVKEY" ]; then
 	echo "SSH Key does not exist"
@@ -25,21 +25,7 @@ echosyntax ()
         echo "Syntax: $0"
 }
 
-if [ "$SRVSTATUS" = "running" ]; then
-	IPv4=`hcloud server ip $SERVERNAME`
-	echo "Cloud node is still running with ip: $IPv4"
-	#if [ -r "$1" ]; then
-	#	echo "sending NZB to server"
-	#	scp "$1" root@$IPv4:"$NZBREMOTEDROPFOLDER"
-	#	exit	
-	#else
-	#	echo "NZB File needed"
-	#	echosyntax
-	#	exit
-	#fi
-fi
-
-hcloud server create --type $SERVERTYPE --name "$SERVERNAME" --image debian-10 --ssh-key $SSHPUBKEY --datacenter fsn1-dc14
+hcloud server create --type "$SERVERTYPE" --name "$SERVERNAME" --image debian-10 --ssh-key $SSHPUBKEY --datacenter fsn1-dc14
 IPv4=`hcloud server ip $SERVERNAME`
 
 ssh-keygen -R "$IPv4"
@@ -47,13 +33,12 @@ ssh-keygen -R "$IPv4"
 SSHUP=255
 while [ $SSHUP != 0 ]; do
 	sleep 1
-	echo "Trying to connect ..."
 	ssh -l root -o="StrictHostKeyChecking off" $IPv4 pwd
 	SSHUP=$?
 done
 
-ssh -l root "$IPv4" "apt --yes update && \
-	apt --yes install git && \
+ssh -l root -i ~/.ssh/id_rsa "$IPv4" "apt -y update && \
+	apt -y install git && \
 	cd /root && \
 	git clone $GITREPO && \
 	cd /root/hcloud-jitsi-builder/ && \
--- run.sh
+++ run.sh
@@ -2,13 +2,15 @@
 
 source ./ENVSETTINGS
 APTINSTALL=""
+
 cd ~
+
+$JITSILINK="https://github.com/$(curl -sL "https://github.com/jitsi/docker-jitsi-meet/releases/latest" | grep .tar.gz | awk -F 'href="' '{print $2}' | awk -F '" rel=' '{print $1}')"
 
 echo "Docker ..."
 if [ ! -x /usr/bin/docker ]; then
 	echo -e "   ${RED}not found${NC} - starting installation!"
-	curl -fsSL https://get.docker.com -o get-docker.sh
-	sudo sh get-docker.sh > /dev/null
+	curl -fsSL https://get.docker.com | bash -
 else
 	echo -e "  ${GREEN}OK${NC} - already installed."
 fi
@@ -16,28 +18,25 @@ fi
 echo "Docker Compose..."
 if [ ! -x /usr/local/bin/docker-compose ]; then
 	echo -e "   ${RED}not found${NC} - starting installation!"
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	sudo chmod +x /usr/local/bin/docker-compose
+	wget -O /usr/local/bin/docker-compose "https://github.com/$(curl -L "https://github.com/docker/compose/releases/latest" | grep -E -m1 "$(uname -s).*$(uname -m)" | awk -F 'href="' '{print $2}' | awk -F '" rel=' '{print $1}')"
+	chmod +x /usr/local/bin/docker-compose
 else
 	echo -e "  ${GREEN}OK${NC} - already installed."
 fi
 
 
 cd ~
-# git clone https://github.com/jitsi/docker-jitsi-meet/releases/latest && cd docker-jitsi-meet
-wget https://github.com/jitsi/docker-jitsi-meet/archive/stable-5142.tar.gz && tar -zxf stable-5142.tar.gz && cd docker-jitsi-meet-stable-5142
+wget -O jitsi-docker.tar.gz "https://github.com/$(curl -sL "https://github.com/jitsi/docker-jitsi-meet/releases/latest" | grep .tar.gz | awk -F 'href="' '{print $2}' | awk -F '" rel=' '{print $1}')"
+mkdir jitsi-docker && tar -xf jitsi-docker.tar.gz --strip-components 1 -C ./jitsi-docker && cd jitsi-docker
 cp env.example .env
 ./gen-passwords.sh
 sed -i 's/#PUBLIC_URL.*$/PUBLIC_URL=$JITSIHOSTNAME/' .env
 sed -i 's/#ENABLE_LETSENCRYPT=1/ENABLE_LETSENCRYPT=1/' .env
-sed -i 's/#LETSENCRYPT_DOMAIN=meet.example.com/LETSENCRYPT_DOMAIN=meet.thomascarduck.de/' .env
-sed -i 's/#LETSENCRYPT_EMAIL=alice@atlanta.net/LETSENCRYPT_EMAIL=thomas.carduck@gmail.com/' .env
+sed -i 's/#LETSENCRYPT_DOMAIN=meet.example.com/LETSENCRYPT_DOMAIN=$JITSIHOSTNAME/' .env
+sed -i 's/#LETSENCRYPT_EMAIL=alice@atlanta.net/LETSENCRYPT_EMAIL=$ENCMAIL/' .env
 sed -i 's/HTTP_PORT=8000/HTTP_PORT=80/' .env
 sed -i 's/HTTPS_PORT=8443/HTTPS_PORT=443/' .env
 sed -i 's/TZ=UTC/TZ=Europe\/Berlin/' .env
 
 mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
 docker-compose up -d
-
-
-	
